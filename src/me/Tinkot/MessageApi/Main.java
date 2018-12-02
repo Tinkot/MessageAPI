4dcd1fcf3af4c7f7ccf7c782ac47afd81462d22f
Tinkot <44869554+Tinkot@users.noreply.github.com>
2018-12-02 23:58:00 +0100

    Update and rename Main.java to src/me/Tinkot/MessageApi/Main.java

--- src/me/Tinkot/MessageApi/Main.java
+++ src/me/Tinkot/MessageApi/Main.java
@@ -4,7 +4,6 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
-
 	public void onEnable() {
         this.getProxy().getPluginManager().registerCommand((Plugin)this, (Command)new CMD());
 	}
